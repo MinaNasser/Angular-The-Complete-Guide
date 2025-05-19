92958af5accf797aea8b50689f87a21fcaf75c35
MinaAl5al <minanasser82018@gmail.com>
2025-05-20 02:51:19 +0300

    Add header and some images and edit style

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [HeaderComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+
+}
