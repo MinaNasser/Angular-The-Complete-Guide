5cde9dcb70228cbd60b8f83ae3d5c16f090daaf5
MinaAl5al <minanasser82018@gmail.com>
2025-05-20 03:53:38 +0300

    onSelectUser

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);// Randomly select a user from the dummy users
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent {}
+export class UserComponent {
+  private selectedUser= DUMMY_USERS[randomIndex]; // Select a random user from the dummy users
+  get user() {
+    return this.selectedUser;
+  }
+  get imagePath() {
+    return `assets/users/${this.selectedUser.avatar}`;
+  }
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex]; // Select a random user from the dummy users
+    console.log('Selected user:', this.selectedUser);
+  }
+}
