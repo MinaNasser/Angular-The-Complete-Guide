6571c4a4414e432106c50c7f0fd2c8f24bc35fd8
MinaAl5al <minanasser82018@gmail.com>
2025-05-22 03:18:14 +0300

    @input

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,9 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, input, Input } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
-import { single } from 'rxjs';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);// Randomly select a user from the dummy users
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);// Randomly select a user from the dummy users
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -11,20 +8,34 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);// Randomly s
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-   selectedUser= signal(DUMMY_USERS[randomIndex]); // Select a random user from the dummy users
-  imagePath =  computed(() => `assets/users/${this.selectedUser().avatar}`);
-   get user() {
-    return this.selectedUser;
+  @Input({required: true}) avatar! : string ;
+  @Input({required: true}) name! : string ;
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => `assets/users/${this.avatar()}`); // Dynamically set the image path based on the avatar property
+
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
   }
 
 
-
-  // get imagePath() {
-  //   return `assets/users/${this.selectedUser().avatar}`;
-  // }
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set( DUMMY_USERS[randomIndex]); // Select a random user from the dummy users
-    console.log('Selected user:', this.selectedUser);
+
   }
+  //  selectedUser= signal(DUMMY_USERS[randomIndex]); // Select a random user from the dummy users
+  // imagePath =  computed(( ) => `assets/users/${this.selectedUser().avatar}`);
+  //  get user() {
+  //   return this.selectedUser;
+  // }
+
+  // // get imagePath() {
+  // //   return `assets/users/${this.selectedUser().avatar}`;
+  // // }
+  // onSelectUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUser.set( DUMMY_USERS[randomIndex]); // Select a random user from the dummy users
+  //   console.log('Selected user:', this.selectedUser);
+  // }
 }
