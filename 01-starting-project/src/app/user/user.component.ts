c405649e8488ceb52a6ff8d2fc5445ac5bbbb29b
MinaAl5al <minanasser82018@gmail.com>
2025-05-25 11:31:48 +0300

    edit

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -11,7 +11,7 @@ export class UserComponent {
   @Input({required: true}) id! : string ;
   @Input({required: true}) avatar! : string ;
   @Input({required: true}) name! : string ;
-  @Output() select =  new EventEmitter();
+  @Output() select =  new EventEmitter<string>();
   // @Output() selectedUserId = new EventEmitter<string>();
 
   selected = output<string>()
