2e9023ec5612eed9a588d5917355f4251376d016
MinaAl5al <minanasser82018@gmail.com>
2025-06-05 00:40:23 +0300

    Hello

--- 01-starting-project/src/app/Tasks/task/task.component.ts
+++ 01-starting-project/src/app/Tasks/task/task.component.ts
@@ -1,12 +1,14 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Task } from 'zone.js/lib/zone-impl';
 import { task } from '../../Models/Task.model';
+import { CardComponent } from "../../shared/card/card.component";
 
 @Component({
   selector: 'app-task',
   standalone: true,
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  styleUrls: ['./task.component.css'],
+  imports: [CardComponent]
 })
 export class TaskComponent implements OnInit {
 
@@ -30,7 +32,7 @@ export class TaskComponent implements OnInit {
   }
 
 
-  
+
   onEditTask() {
     console.log(`Task with ID ${this.task.id} edited.`);
   }
