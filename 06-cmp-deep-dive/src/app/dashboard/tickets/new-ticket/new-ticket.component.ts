8f308f15833dd6bb9dcf4fa41553527362f57929
MinaAl5al <minanasser82018@gmail.com>
2025-06-11 02:58:12 +0300

    Custom Button

--- 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MyButtonComponent } from "../../../shared/my-button/my-button.component";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [],
+  imports: [MyButtonComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
--- 06-cmp-deep-dive/src/app/header/header.component.ts
+++ 06-cmp-deep-dive/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MyButtonComponent } from "../shared/my-button/my-button.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MyButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
