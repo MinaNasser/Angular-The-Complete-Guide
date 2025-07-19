157304e77ece49264e04d71908cee2385b35875e
MinaAl5al <minanasser82018@gmail.com>
2025-07-20 02:56:36 +0300

    app-control

--- 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 
-import { MyButtonComponent } from "../../../shared/my-button/my-button.component";
+import { MyButtonComponent } from '../../../shared/my-button/my-button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [MyButtonComponent],
+  imports: [MyButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-   
-
-}
+export class NewTicketComponent {}
--- 06-cmp-deep-dive/src/app/shared/my-button/my-button.component.ts
+++ 06-cmp-deep-dive/src/app/shared/my-button/my-button.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  selector: 'button[appButton] , a[appButton] , input[appButton]',
   // button[appButton]
   standalone: true,
   imports: [],
   templateUrl: './my-button.component.html',
-  styleUrl: './my-button.component.css'
+  styleUrl: './my-button.component.css',
 })
-export class MyButtonComponent {
-
-}
+export class MyButtonComponent {}
