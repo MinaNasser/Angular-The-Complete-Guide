c0a93e954ed48235a0c024c742c33b6d1e09894c
MinaAl5al <minanasser82018@gmail.com>
2025-08-17 01:00:39 +0300

    HostListener & HostBinding

--- 06-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
   HostBinding,
+  HostListener,
   input,
   ViewEncapsulation,
 } from '@angular/core';
@@ -12,13 +13,21 @@ import {
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
-  // host: {
-  //   class: 'control',
-  //   '[class.control--required]': 'label.required',
-  //   '[class.control--invalid]': 'label.invalid',
-  // },
+  host: {
+    class: 'control',
+    '[class.control--required]': 'label.required',
+    '[class.control--invalid]': 'label.invalid',
+    // '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
-  @HostBinding('class') className = 'control';
+  // @HostBinding('class') className = 'control';
+  @HostListener('click') onClick(): void {
+    console.log('Control clicked');
+  }
+
   label = input.required<string>();
+  // onClick(): void {
+  //   console.log('click');
+  // }
 }
