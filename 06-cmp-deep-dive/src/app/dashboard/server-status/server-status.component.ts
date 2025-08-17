071eb36271fbc57846757e355d7adce66f95425b
MinaAl5al <minanasser82018@gmail.com>
2025-08-18 02:18:00 +0300

    Array<'online' | 'offline' | 'unknown'>

--- 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,34 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'unknown';
 
-  getStatus() {
-    // Simulate fetching server status
-    return this.currentStatus;
+  constructor() {}
+
+  ngOnInit(): void {
+    // const statuses: Array<'online' | 'offline' | 'unknown'> = [
+    //   'online',
+    //   'offline',
+    //   'unknown',
+    // ];
+    // this.currentStatus = statuses[Math.floor(Math.random() * statuses.length)];
+    // // Simulate a server status check
+    // setTimeout(() => {
+    //   // Randomly change the status after 2 seconds
+    //   this.currentStatus =
+    //     statuses[Math.floor(Math.random() * statuses.length)];
+    // }, 2000);
+    setInterval(() => {
+      // Simulate changing server status every 2 seconds  (online/offline/unknown)
+      const statuses: ('online' | 'offline' | 'unknown')[] = [
+        'online',
+        'offline',
+        'unknown',
+      ];
+      this.currentStatus =
+        statuses[Math.floor(Math.random() * statuses.length)];
+      // console.log(`Server status changed to: ${this.currentStatus}`);
+    }, 5000);
   }
 }
