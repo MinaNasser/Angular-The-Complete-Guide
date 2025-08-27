e443dcb664cf5b21d29cc0bdf3addc3616ac7f7b
MinaAl5al <minanasser82018@gmail.com>
2025-08-28 00:30:13 +0300

    sdds

--- 06-cmp-deep-dive/src/app/header/header.component.ts
+++ 06-cmp-deep-dive/src/app/header/header.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 
-import { MyButtonComponent } from "../shared/my-button/my-button.component";
+import { MyButtonComponent } from '../shared/my-button/my-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [MyButtonComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  // no logic yet
+  //
 }
