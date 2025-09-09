aca692a833b19df82decb5ebaeeb479212cf010a
MinaAl5al <minanasser82018@gmail.com>
2025-09-10 00:53:18 +0300

    ngSubmit

--- 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 
 import { MyButtonComponent } from '../../../shared/my-button/my-button.component';
@@ -6,8 +7,12 @@ import { ControlComponent } from '../../../shared/control/control.component';
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [MyButtonComponent, ControlComponent],
+  imports: [MyButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  onSubmit(): void {
+    console.log('Form submitted');
+  }
+}
