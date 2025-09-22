0d3a542f85c309abba64bda6039ddf06a2f0a1d0
MinaAl5al <minanasser82018@gmail.com>
2025-09-22 04:03:04 +0300

    afterNextRender

--- 06-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -4,7 +4,6 @@ import {
   Component,
   contentChild,
   ElementRef,
-  HostBinding,
   HostListener,
   inject,
   input,
@@ -42,10 +41,15 @@ export class ControlComponent {
    *
    */
   constructor() {
+    // afterRender is a lifecycle hook that runs after the component's view has been rendered
+    // afterNextRender is a lifecycle hook that runs after the component's view has been rendered and the next change detection cycle has completed
+
     afterRender(() => {
       console.log('After render');
     });
     console.log('Constructor');
+
+    // afterNextRender is a lifecycle hook that runs after the component's view has been rendered and the next change detection cycle has completed
     afterNextRender(() => {
       console.log('After next render');
     });
