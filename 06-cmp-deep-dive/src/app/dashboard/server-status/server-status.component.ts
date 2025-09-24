6527f38e8947b2692aed01fdf4ac9faf85a74f32
MinaAl5al <minanasser82018@gmail.com>
2025-09-24 23:31:55 +0300

    czxc

--- 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -37,8 +37,6 @@ export class ServerStatusComponent implements OnInit, AfterViewInit {
     this.destroyRef.onDestroy(() => clearInterval(interval));
   }
   ngAfterViewInit(): void {
-    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    //Add 'implements AfterViewInit' to the class.
     console.log('ServerStatusComponent view initialized.');
   }
 }
