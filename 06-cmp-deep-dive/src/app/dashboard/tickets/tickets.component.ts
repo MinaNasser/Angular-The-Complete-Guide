edea85e38b3afdb5b11522b35ecabf2173fd12a4
MinaAl5al <minanasser82018@gmail.com>
2025-09-26 01:50:41 +0300

    Ticket

--- 06-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
@@ -1,11 +1,12 @@
 import { Ticket } from './ticket.model';
 import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
