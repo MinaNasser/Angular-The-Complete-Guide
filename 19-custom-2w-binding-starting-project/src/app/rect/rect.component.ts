b753ecdd6231d1b86eefb589843197cd89db640f
MinaAl5al <minanasser82018@gmail.com>
2025-10-09 01:33:20 +0300

    ZXZ

--- 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -9,17 +9,8 @@ import { Component } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  width = 200;
-  height = 100;
-  color = 'blue';
-  backgroundColor = 'white';
-  borderRadius = 0;
 
   onReset() {
-    this.width = 200;
-    this.height = 100;
-    this.color = 'blue';
-    this.backgroundColor = 'white';
-    this.borderRadius = 0;
+    // ...
   }
 }
