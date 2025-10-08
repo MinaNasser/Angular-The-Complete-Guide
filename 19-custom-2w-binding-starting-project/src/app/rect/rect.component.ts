759499c7e9ffb4c2fe5a340186e957cc196b761f
MinaAl5al <minanasser82018@gmail.com>
2025-10-09 02:50:06 +0300

    finish 06-cmp-deep-dive

--- 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -12,8 +12,10 @@ export class RectComponent {
 
   @Input({ required: true }) size!: { width: string; height: string };
   @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+  size2 = model.required<{ width: string; height: string }>();
 
   onReset() {
     this.sizeChange.emit({ width: '100', height: '100' });
+    this.size2.set({ width: '100', height: '100' });
   }
 }
