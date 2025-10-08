2b7ed88d41a8272a054a86b354fb672bb3a5f1dc
MinaAl5al <minanasser82018@gmail.com>
2025-10-09 02:38:58 +0300

    rECAT

--- 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -10,6 +10,7 @@ import { Component } from '@angular/core';
 export class RectComponent {
   // Todo: Implement custom two-way binding
 
+  @Input({ required: true }) size!: { width: string; height: string };
   onReset() {
     // ...
   }
