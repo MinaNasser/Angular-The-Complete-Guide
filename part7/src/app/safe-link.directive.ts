7f1f1ed55221ae574a3e1a8414016536844a0b64
MinaAl5al <minanasser82018@gmail.com>
2025-10-13 01:41:55 +0300

    Event  in Directive

--- part7/src/app/safe-link.directive.ts
+++ part7/src/app/safe-link.directive.ts
@@ -3,6 +3,9 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onClick($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
