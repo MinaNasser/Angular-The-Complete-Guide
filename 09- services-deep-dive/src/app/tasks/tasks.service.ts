894c2dea0cd2c6f23ebdcae659a54ee9c57034c1
MinaAl5al <minanasser82018@gmail.com>
2025-12-01 11:43:06 +0200

    LoggingService inject in TaskService

--- 09- services-deep-dive/src/app/tasks/tasks.service.ts
+++ 09- services-deep-dive/src/app/tasks/tasks.service.ts
@@ -1,3 +1,4 @@
+import { LoggingService } from '../logging.service';
 import { task } from './../../../../01-starting-project/src/app/Models/Task.model';
 import { AddTaskDTO, Task, TaskStatus } from './task.model';
 import { Injectable, signal } from '@angular/core';
@@ -6,6 +7,7 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root',
 })
 export class TasksService {
+  constructor(private logs: LoggingService) {}
   private tasks = signal<Task[]>([]);
 
   addTask(taskData: AddTaskDTO) {
@@ -15,6 +17,12 @@ export class TasksService {
       status: 'OPEN',
     };
     this.tasks.update((oldTasks) => [...oldTasks, newTasks]);
+    this.logs.log(
+      'Task Added! Title: ' +
+        taskData.title +
+        ', Description: ' +
+        taskData.description
+    );
   }
 
   updateTaskStatus(taskId: string, newStatus: TaskStatus) {
@@ -23,14 +31,19 @@ export class TasksService {
         task.id === taskId ? { ...task, status: newStatus } : task
       )
     );
+    this.logs.log(
+      'Task Status Updated! ID: ' + taskId + ', New Status: ' + newStatus
+    );
   }
   // ()() to call   the signal and get the value
   getTaskById(taskId: string) {
+    this.logs.log('Task Retrieved! ID: ' + taskId);
     return this.tasks
       .asReadonly()()
       .find((task) => task.id === taskId);
   }
   getAllTasks() {
+    this.logs.log('All Tasks Retrieved!');
     return this.tasks.asReadonly();
   }
 }
